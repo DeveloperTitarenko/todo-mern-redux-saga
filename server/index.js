94987a23d615df3f855da5a4c7dda2a4cf125a3f
Vlad <developertitarenko@gmail.com>
2021-07-05 16:30:04 +0300

    heroku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require("cors")
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path');
 
 const app = express()
 const authRouter = require('./routes/auth.router')
@@ -9,6 +10,11 @@ const taskRouter = require('./routes/tasks.router')
 const userRouter = require('./routes/user.router')
 
 const PORT = process.env.PORT || 5000
+
+app.use(express.static('../client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
